7d270474516f8c23a30dcb8b3943bd5f9316f875
nagarajp20042606 <nouse200426@gmail.com>
2025-09-26 12:24:04 +0530

    addede the csrf tokends

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = 'login'
+
+CSRF_TRUSTED_ORIGINS = ['https://work-nest-django.azurewebsites.net']
